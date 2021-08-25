9a24c888b049b715b92116cfd81a3ad74188eb36
Md Rayhanul Islam <73143090+rayhanul2008@users.noreply.github.com>
2021-08-26 00:41:46 +0600

    Update GuessTheMovie.java

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,22 +1,20 @@
 import java.util.*;
 public class GuessTheMovie {
     public static void main(String[] args) {
-        String[] movie = {"mucize", "rocky", "all in one", "who am i", "the matrix", "monpura", "train to busan", "hope2013", "a moment to remember", "monpura", "3idots",
-                "messenger", "a beautiful mind", "life is beautiful", "taqdir", "hridoyer kotha", "no win", "successful", "the lord of the rings", "die hard"};
+        String[] movie = {"mucize","Interstellar","American History X","Avengers Endgame","The Sting","rocky", "all in one","The Lord of the Rings", "who am i", "the matrix", "monpura", "train to busan", "hope2013", "a moment to remember", "monpura", "3idots",
+                "messenger", "a beautiful mind", "life is beautiful","The Dark Knight", "taqdir","Alien","Hamilton", "hridoyer kotha", "no win", "successful", "the lord of the rings", "die hard"};
+
         int size = movie.length;
-
         Random random = new Random();           // random number
+
         int number = random.nextInt(size);
-        System.out.println(number);
+        int size1 = movie[number].length();     // select random movie length
 
-        int size1 = movie[number].length();        // select random movie length
-        System.out.println(size1);
+        String str = movie[number];             // random movie name
 
-        String str = movie[number];                 // random movie name
         char[] outputMovie = new char[size1];
-
         String expecteedMovie = movie[number];
-
+        System.out.print("You are Guessing:");
         for (int i = 0; i < size1; i++) {
             if(expecteedMovie.charAt(i)!=' ') {
                 outputMovie[i] ='_';
@@ -26,36 +24,37 @@ public class GuessTheMovie {
             }
         }
         System.out.println(outputMovie);
-        int t = 0;
         Scanner sc = new Scanner(System.in);
+        int t = 0;
         while (t < 10) {
+            System.out.println("You have guessed (" + t + ") wrong letters:");
             System.out.print("Guess a Letter:");
             char userInput = sc.next().charAt(0);
-            boolean matched = false;
+            boolean result = false;
             for (int i = 0; i < size1; i++) {
                 if (userInput == expecteedMovie.charAt(i)){
                     outputMovie[i] = userInput;
-                    matched=true;
+                    result = true;
                 }
             }
-            if(matched==false){
+            if(result == false){
                 t++;
             }
-            System.out.println("You have guessed (" + t + ") wrong letters");
+            System.out.print("You are Guessing:");
             System.out.println(outputMovie);
-            boolean success = true;
+            boolean result1 = true;
             for (int i = 0; i < size1; i++) {
                if(outputMovie[i]=='_'){
-                   success= false;
+                   result1 = false;
                }
             }
-            if(success == true){
-                System.out.println("Congratulations,YOu Win! ✔✔");
+            if(result1 == true){
+                System.out.println("Wow! Congratulations! You Win!");
                 return;
             }
 
         }
-        System.out.println("You Loose!");
+        System.out.println("OOPS! You Lost It!");
 
     }
 }
